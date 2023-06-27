f8c20f191428ea0068a248bf1414b72fa4da2174
Martin <martin.praetzlich@outlook.de>
2023-06-27 16:51:25 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="app">
+        <header>
+            <img alt="Vue logo" src="./assets/logo.png">
+            <HelloWorld msg="Welcome to Your Vue.js App" />
+            <!-- Hier könnten Sie eine Navigationsleiste oder ein Logo platzieren -->
+        </header>
+
+        <aside>
+            <!-- Hier könnten Sie eine Seitenleiste für zusätzliche Informationen oder Links platzieren -->
+        </aside>
+
+        <main>
+            <BluetoothDevices />
+        </main>
+
+        <footer>
+            <!-- Hier könnten Sie Copyright-Informationen oder Links zu Datenschutzrichtlinien platzieren -->
+        </footer>
+    </div>
+</template>  
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+    name: 'App',
+    components: {
+        HelloWorld
+    }
+}
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer>
+        <a id="copyright" href="https://discord.com/invite/DXWuZUBW2C" target="_blank" rel="noreferrer">
+            Copyright ©️ Martin Prätzlich 2023 - {{ year }} BluetoothApp
+        </a>
+    </footer>
+</template>
+  
+<script>
+export default {
+    data() {
+        return {
+            year: new Date().getFullYear()
+        }
+    }
+}
+</script>  
