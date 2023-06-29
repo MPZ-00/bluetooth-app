d10dd14c3394cc930d9252be89f671bbe60e96a1
Martin <martin.praetzlich@outlook.de>
2023-06-29 21:17:56 +0200

    new approach

---
+++ src/components/BluetoothDevice.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div>
+            {{ getDeviceName(device) }}
+            <button v-if="!device.name" @click="setDeviceName" class="info">Set Name</button>
+            <button v-if="!device.connected" @click="connectToDevice" class="success">Connect</button>
+            <button v-else @click="disconnectFromDevice" class="danger">Disconnect</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        device: Object,
+    },
+    methods: {
+        getDeviceName() {
+            return this.device.name || 'No Name'
+        },
+        setDeviceName() {
+            let newName = prompt('Enter new device name')
+            if (newName) {
+                this.$emit('set-name', this.device, newName)
+            }
+        },
+        disconnectFromDevice() {
+            this.$emit('disconnect', this.device)
+        },
+        connectToDevice() {
+            this.$emit('connect', this.device)
+        },
+    },
+}
+</script>
--- src/components/BluetoothDevices.vue
+++ src/components/BluetoothDevices.vue
@@ -4,11 +4,12 @@
         <button @click="connect" class="warning">Connect to a Device</button>
         <ul>
             <li v-for="device in devices" :key="device.id">
-                {{ getDeviceName(device) }}
-                <button v-if="!device.name" @click="setDeviceName(device)" class="info">Set
-                    Name</button>
-                <button v-if="!device.connected" @click="connectToDevice(device)" class="success">Connect</button>
-                <button v-else @click="disconnectFromDevice(device)" class="danger">Disconnect</button>
+                <BluetoothDevice 
+                    :device="device"
+                    @set-name="setDeviceName"
+                    @connect="connectToDevice"
+                    @disconnect="disconnectFromDevice"
+                />
             </li>
         </ul>
     </div>
@@ -16,18 +17,20 @@
   
 <script>
 import axios from 'axios'
+import BluetoothDevice from './BluetoothDevice.vue'
 const PORT = process.env.VUE_APP_PORT || 3000
 
 export default {
+    components: {
+        BluetoothDevice,
+    },
     data() {
         return {
             devices: [],
-            deviceNames: {},
         }
     },
     mounted() {
         this.fetchDevices()
-        console.log('this.devices', this.devices)
     },
     methods: {
         async fetchDevices() {
@@ -53,55 +56,6 @@ export default {
                 this.devices.push(newDevice)
             } catch (error) {
                 console.error('Error adding device:', error)
-            }
-        },
-        async connect() {
-            try {
-                let requestedDevice = await navigator.bluetooth.requestDevice({
-                    acceptAllDevices: true,
-                    optionalServices: ['battery_service']
-                })
-                console.log('requestedDevice', requestedDevice)
-
-                if (!this.devices.find(device => device.id === requestedDevice.id)) {
-                    this.devices.push(requestedDevice)
-                }
-            } catch (error) {
-                if (error instanceof DOMException && error === 20) {
-                    console.log('User cancelled the requestDevice() chooser.')
-                } else {
-                    console.error('Error connecting to device:', error)
-                }
-            }
-        },
-        async setDeviceName(device) {
-            let newName = prompt('Enter new device name')
-            if (newName) {
-                this.deviceNames[device.id] = newName
-                await this.saveDevice(device)
-            }
-        },
-        getDeviceName(device) {
-            return device.name || this.deviceNames[device.id] || 'No Name'
-        },
-        disconnectFromDevice(device) {
-            console.log('disconnectFromDevice', device)
-            try {
-                if (device.gatt.connected) {
-                    device.gatt.disconnect()
-                    device.connected = false
-                }
-            } catch (error) {
-                console.error('Error disconnecting from device:', error)
-            }
-        },
-        connectToDevice(device) {
-            console.log('connectToDevice', device)
-            try {
-                device.gatt.connect()
-                device.connected = true
-            } catch (error) {
-                console.error('Error connecting to device:', error)
             }
         },
     },
