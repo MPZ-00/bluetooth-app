5d4746f80f3ed6306e1df2edd429e90e7e462e59
Martin <martin.praetzlich@outlook.de>
2023-06-30 14:10:25 +0200

    added form with services and charasteristics

--- src/components/BluetoothDataSender.vue
+++ src/components/BluetoothDataSender.vue
@@ -1,25 +1,43 @@
 <template>
-    <div>
-        <button @click="sendData">Send Data</button>
-    </div>
+    <form>
+        <input v-model="service" type="text" list="services" autofocus placeholder="Bluetooth Service">
+        <datalist id="services">
+            <option v-for="service in servicesList" :value="service" :key="service">
+                {{ service }}
+            </option>
+        </datalist>
+
+        <input v-model="characteristic" type="text" list="characteristics" placeholder="Bluetooth Characteristic">
+        <datalist id="characteristics">
+            <option v-for="characteristic in characteristicsList" :value="characteristic" :key="characteristic">
+                {{ characteristic }}
+            </option>
+        </datalist>
+
+        <button @click.prevent="onStartButtonClick">Start notifications</button>
+        <button @click.prevent="onStopButtonClick">Stop notifications</button>
+    </form>
 </template>
 <script>
+import characteristicsList from '../utils/charasteristics.js'
+import servicesList from '../utils/services.js'
 
 export default {
     name: 'BluetoothDataSender',
     data() {
         return {
-            devices: null, // Das verbundene BT Gerät
-            server: null,
             service: null,
             characteristic: null,
+            myCharacteristic: null,
+            servicesList,
+            characteristicsList
         }
     },
     methods: {
         async sendData() {
             console.log('sending data')
-            this.service = await this.server.getPrimaryService('0000ffe0-0000-1000-8000-00805f9b34fb')
-            this.characteristic = await this.service.getCharacteristic('0000ffe1-0000-1000-8000-00805f9b34fb')
+            this.service = await this.server.getPrimaryService('battery_service')
+            this.characteristic = await this.service.getCharacteristic('battery_level')
 
             var data = new TextEncoder().encode('Hello, Bluetooth!')
 
